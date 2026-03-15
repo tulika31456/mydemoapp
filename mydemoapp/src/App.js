55cdcc6f94ade4bd881c484968cced432eabd324
Tulika Biswas <tb9092333@gmail.com>
2026-03-15 08:53:49 +0530

    feat: text changed

--- mydemoapp/src/App.js
+++ mydemoapp/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           This is my feature branch code change. I am learning how to use git and github.
         </p>
         <a
           className="App-link"
